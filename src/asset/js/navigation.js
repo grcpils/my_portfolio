75f6a7e48a626eeef4ea1e595e023ca4685c7815
Pierrick Gouerec <pgouerec@adfinitas.fr>
2020-08-28 18:21:20 +0200

    [+] Adding responsiv navigation

--- src/asset/js/navigation.js
+++ src/asset/js/navigation.js
@@ -12,3 +12,23 @@ window.onscroll = function() {
     $('#nav-download').fadeOut()
   }
 };
+
+$('.toggler').click(function() {
+  if ($('.navigation__collapse').hasClass('collapsed')) {
+    $('.toggler').removeClass('active');
+    $('.navigation').removeClass('collapsed')
+    $('.navigation__collapse').removeClass('collapsed')
+  } else {
+    $('.toggler').addClass('active');
+    $('.navigation__collapse').addClass('collapsed')
+    $('.navigation').addClass('collapsed')
+  }
+});
+
+$('.menu-item').each(function() {
+  $(this).click(function() {
+    $('.toggler').removeClass('active');
+    $('.navigation').removeClass('collapsed')
+    $('.navigation__collapse').removeClass('collapsed')
+  })
+})
