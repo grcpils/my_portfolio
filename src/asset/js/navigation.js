9eac049812751e22abc7eed80e4ffded9fafb91a
Pierrick Gouerec <pgouerec@adfinitas.fr>
2020-08-28 15:45:29 +0200

    [+] Adding basic structure

---
+++ src/asset/js/navigation.js
@@ -0,0 +1,14 @@
+let headOffSet = $('#header-download')[0].offsetTop;
+window.onscroll = function() {
+  if (window.pageYOffset > headOffSet) {
+    $('#nav-download').show()
+    $('#nav-download').addClass('button--nav-view');
+  } else {
+    $('#nav-download').removeClass('button--nav-view');
+    $('#nav-download').fadeOut()
+  }
+};
+
+$(document).ready(function() {
+  $('#nav-download').hide();
+})
